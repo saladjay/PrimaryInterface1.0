2f1b9277aee1356834ccdecac18b9ebda6e079ab
yongjiedeng <yongjiedeng@PCTC012>
2017-09-28 20:09:58 +0800

    i dont know where i change. but it is ok to run

--- PrimaryInterface1.0/Controls/ItemsGrid.cs
+++ PrimaryInterface1.0/Controls/ItemsGrid.cs
@@ -4,6 +4,7 @@ using PrimaryInterface1._0.Model;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Linq;
@@ -87,4 +88,85 @@ namespace PrimaryInterface1._0.Controls
         }
     }
 
+    public class ExtendedItemsSourceComponent : DependencyObject
+    {
+        [CommonDependencyProperty]
+        public static readonly DependencyProperty ExtendedItemsSourceProperty = DependencyProperty.Register("ExtendedItemsSource", 
+            typeof(IEnumerable), typeof(ExtendedItemsSourceComponent), 
+            new PropertyMetadata(((IEnumerable)null), new PropertyChangedCallback(OnExtendedItemsSourceChanged)));
+
+        private static void OnExtendedItemsSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ExtendedItemsSourceComponent tempComponent = d as ExtendedItemsSourceComponent;
+            IEnumerable oldValue = (IEnumerable)e.OldValue;
+            IEnumerable newValue = (IEnumerable)e.NewValue;
+            tempComponent.OnExtendedItemsSourceChanged(oldValue, newValue);
+            tempComponent.ExtendedItemsSourceEvent?.Invoke(oldValue, newValue);
+        }
+
+        public delegate void ExtendedItemsSourceEventHandler(IEnumerable oldValue, IEnumerable newValue);
+        public event ExtendedItemsSourceEventHandler ExtendedItemsSourceEvent;
+
+        protected virtual void OnExtendedItemsSourceChanged(IEnumerable oldValue,IEnumerable newValue)
+        {
+
+        }
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ExtendedItemsSourceProperty); }
+            set
+            {
+                if (value == null)
+                {
+                    ClearValue(ExtendedItemsSourceProperty);
+                }
+                else
+                {
+                    SetValue(ExtendedItemsSourceProperty, value);
+                }
+            }
+        }
+
+        public ExtendedItemsSourceComponent()
+        {
+            
+        }
+    }
+
+    public class AttachedItemsSourceComponent
+    {
+        public delegate void AttachedItemsSourceChangedEventHandler(IEnumerable oldValue,IEnumerable newValue);
+        public event AttachedItemsSourceChangedEventHandler AttachedItemsSourceChanged;
+        public delegate void AttachedItemsChangedHandler(object item, bool IsAdd);
+        public event AttachedItemsChangedHandler AttachedItemsChanged;
+
+        private ObservableCollection<object> _attachedItemsSource;
+        public ObservableCollection<object> AttachedItemsSource
+        {
+            get { return _attachedItemsSource; }
+            set
+            {
+                AttachedItemsSourceChanged?.Invoke(_attachedItemsSource,value);
+                _attachedItemsSource = value;
+            }
+        }
+
+        public AttachedItemsSourceComponent()
+        {
+            _attachedItemsSource.CollectionChanged += _attachedItemsSource_CollectionChanged;
+        }
+
+        private void _attachedItemsSource_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            foreach (var item in e.NewItems)
+            {
+                AttachedItemsChanged?.Invoke(item, true);
+            }
+            foreach (var item in e.OldItems)
+            {
+                AttachedItemsChanged?.Invoke(item, false);
+            }
+        }
+    }
 }
